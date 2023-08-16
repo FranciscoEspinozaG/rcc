b3bf81b2e324b5a617e3a4d52b676535e3181ec1
Francisco <francisco_e3@hotmail.com>
2023-08-16 16:48:11 -0400

    cambio a carpeta para visualizar el sitio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SliderHome, MenuHeader, FooterSite, SeoHomeSite, MapaTaller, MainSection } from './components';
+import { SliderHome, MenuHeader, FooterSite, MainSection } from './components';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './assets/css/custom.css';
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <SliderHome/>
     <MainSection/>
     <FooterSite/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
