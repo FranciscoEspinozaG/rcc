6d8e143a663a33137bdb8ca2175149cee02ec2ed
Francisco <francisco_e3@hotmail.com>
2023-08-18 23:58:03 -0400

    cambio para slides, consulta desde un json

--- src/components/SliderHome.jsx
+++ src/components/SliderHome.jsx
@@ -1,7 +1,11 @@
 import Slider from 'react-slick'
 import { SlideItem } from './index'
+import { useState } from 'react'
 
 export const SliderHome = () => {
+
+    const [servicios, setServicios] = useState([]);
+
     let setting = {
         dots: false,
         arrows: false,
@@ -12,41 +16,25 @@ export const SliderHome = () => {
         slidesToScroll: 1
     }
 
-    let servicios = [
-        {
-            titulo: 'Motogrúa',
-            bajada: 'Traslado en la RM y al rededores',
-            img: './img/hombre-intenta-arreglar-moto-al-costado-carretera.jpg' ,
-            textoBoton: 'Agendar',
-            textoWsp: 'Hola, necesito el servicio de Motogrúa, tienes disponibilidad?',
-            activo: true
-        },
-        {
-            titulo: 'Escanner a domicilio',
-            bajada: 'Todas las marcas y modelos | OBD II',
-            img: './img/mechanic-tools.jpg',
-            textoBoton: 'Consultar',
-            activo: true
-        },{
-            titulo: 'Servicio de Revisión Técnica',
-            bajada: 'Gestión completa desde la puerta de su casa',
-            img: './img/mechanic-work.jpg',
-            activo: true
+    const getServicios = async()=>{
+        try{
+            const response = await fetch('http://localhost:5173/data/servicios.json');
+            const data = await response.json();
+            setServicios(data);
+        }catch (error){
+            console.log(error);
         }
-        // ,{
-        //     titulo: 'Electricidad Automotriz',
-        //     bajada: 'Todo tipo de cableado para tu vehículo',
-        //     activo: false
-        // }
-    ]
+    }
+
+    getServicios();
 
     return (
         <>
         <Slider {...setting} className='slider-home'>
             {
-                servicios.map( ({titulo, bajada, img, textoBoton, textoWsp}) => (
+                servicios.map( ({titulo, bajada, img, textoBoton, textoWsp, id}) => (
                     <SlideItem
-                        key={titulo}
+                        key={id}
                         titulo={titulo}
                         bajada={bajada}
                         img={img}
